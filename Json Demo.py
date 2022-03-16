1ec612155033898b71745bd1a66c30f906590c2b
hidirgungor <hidirgungor@icloud.com>
2022-03-16 11:56:38 +0300

    Json Demo Register

--- Json Demo.py
+++ Json Demo.py
@@ -1,4 +1,7 @@
+from hashlib import new
 import json
+import os
+from psutil import users
 
 class User:
     def __init__(self, username, password, email):
@@ -12,27 +15,50 @@ class UserRepository:
         self.users = []
         self.isLoggedIn = False
         self.current:User = {}
-        self.loadUser()
+        self.loadUsers()
     
-    def loadUser(self):
-        pass
+    def loadUsers(self):
+        if os.path.exists('users.json'):
+            with open('users.json','r', encoding='utf=8') as file:
+                users = json.load(file)
+                for user in users:
+                    user = json.loads(user)
+                    newUser = User(username = user['username'], password = user['password'], email = user['email'])
+                    self.users.append(newUser)
+                    #print(user['username'])
+            print(self.users)
 
     def register(self, user: User):
         self.users.append(user)
+        self.savetoFile()
         print('Kullanıcı Oluşturuldu.')
-    def login(self):
-        pass
+   
+    def login(self, username, password):
+            for user in self.users:
+                if user.username == username and user.password == password:
+                    self.isLoggedIn = True
+                    self.current = user
+                    print('Giriş Yapıldı.')
+                    break
+    def logout(self):
+        self.isLoggedIn = False
+        self.current = {}
+        print('Çıkış Yapıldı.')
+
+    def identity(self):
+        if self.isLoggedIn:
+            print(f'username: {self.current.username}')
+        else:
+            print('giriş yapılmadı.')
+            
     def savetoFile(self):
         list = []
-
         for user in self.users:
-            list.append(json.dumps(user.__dict__))
-            
+            list.append(json.dumps(user.__dict__))      
         with open('users.json','w') as file:
-            json.dumps(list, file)
+            json.dump(list, file)
 
 reposiory = UserRepository()
-
 
 while True:
     print('Menü'.center(50,'*'))
@@ -44,17 +70,20 @@ while True:
             username = input('username: ')
             password = input('password: ')
             email = input('email: ')
-
             user = User(username=username, password=password, email=email)
             reposiory.register(user)
-
             #print(reposiory.users)
         elif secim == '2':
-            pass
+            if reposiory.isLoggedIn:
+                print('Zaten giriş yapılmış')
+            else:
+                username = input('username: ')
+                password = input('password: ')
+                reposiory.login(username, password)
         elif secim == '3':
-            pass
+            reposiory.logout()
         elif secim == '4':
-            pass
+            reposiory.identity()
         else:
             print('Yanlış Seçim')
 
